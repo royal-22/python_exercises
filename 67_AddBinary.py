532223e6e8aeb06821fabdf38e9d1def541b5b65
royal-22 <87497586+royal-22@users.noreply.github.com>
2022-07-25 16:51:06 -0500

    Create 67_AddBinary.py

---
+++ 67_AddBinary.py
@@ -0,0 +1,28 @@
+"""
+Given two binary strings a and b, return their sum as a binary string.
+source: https://leetcode.com/problems/add-binary/
+:)
+"""
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return bin(int(a, 2) + int(b, 2))[2:]
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = ""
+        carry = 0
+        
+        a, b = a[::-1], b[::-1]
+        
+        for i in range(max(len(a), len(b))):
+            digitA = ord(a[i]) - ord("0") if i < len(a) else 0
+            digitB = ord(b[i]) - ord("0") if i < len(b) else 0
+            total = digitA + digitB + carry
+            char = str(total%2) # 1%2 = 1  // 2%2=0  // 3%2=1
+            
+            res = char + res
+            carry = total // 2
+        if carry: 
+            res = "1" + res
+        return res
